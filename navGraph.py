75c03b5d06fb052b5f65f37829ec11a2ba70b5db
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2025-05-31 23:59:05 -0500

    todo

--- navGraph.py
+++ navGraph.py
@@ -1,19 +1,48 @@
+from viewBase import viewBase
+import pygame
+from enum import Enum
+
+class Direction(Enum):
+    Next = 1
+    Back = 2
+    Modes = 3
+    Partidas = 4
+    Again = 5
+
 class NavigationGraph:
-    graph: dict = {}
-    
+    views = []
+    #TODO: Add a dictionary to hold transitions between views
+
     def __init__(self):
-        self.graph = {}
+        pass
 
-    def add_node(self, node):
-        if node not in self.graph:
-            self.graph[node] = []
+    def addView(self, view):
+        self.views.append(view)
+    
+    def addTransition(srcView: viewBase, destView: viewBase, direction: Direction):
+        if srcView not in NavigationGraph.views:
+            NavigationGraph.views.append(srcView)
 
-    def add_edge(self, from_node, to_node):
-        if from_node in self.graph and to_node in self.graph:
-            self.graph[from_node].append(to_node)
+        if destView not in NavigationGraph.views:
+            NavigationGraph.views.append(destView)
 
-    def get_neighbors(self, node):
-        return self.graph.get(node, [])
-
-    def __str__(self):
-        return str(self.graph)
+    def go(view: viewBase, direction: Direction):
+        if direction == Direction.Next:
+            # Logic to go to the next view
+            pass
+        elif direction == Direction.Back:
+            # Logic to go back to the previous view
+            pass
+        elif direction == Direction.Modes:
+            # Logic to switch to modes view
+            pass
+        elif direction == Direction.Partidas:
+            # Logic to switch to partidas view
+            pass
+        elif direction == Direction.Again:
+            # Logic to restart the current view
+            pass
+        else:
+            raise ValueError("Invalid navigation direction")
+        
+        tempView = views[]
